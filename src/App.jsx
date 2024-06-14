08d3f1a5b62c52a9e160038e01bdc5e81963cee2
AndreeaSB <andreea.sbadea@gmail.com>
2024-06-14 19:36:35 +0300

    doesn.t work. must find out WHY

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,100 @@
-import './App.css'
-import Board from './componets/Board'
-import Keyboard from './componets/Keyboard'
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import Board from "./componets/Board";
+import Keyboard from "./componets/Keyboard";
+import GameOver from "./componets/GameOver"
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import { boardDefault, generateWordSet } from "./Words";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([])
+  const [gameOver, setGameOver] = useState({gameOver: false, guessedWord: false})
+  const [correctWord, setCorrectWord] = useState("")
 
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      console.log(words.todaysWord)
+      setCorrectWord(words.todaysWord)
+    });
+  }, []);
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letter] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+    console.log(board)
+  };
+  const onEnter = () => {
+    if (currAttempt.letter !== 5) return;
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+     currWord += "\r"
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letter: 0 });
+    } else {
+      alert("Word not found");
+    }
+
+    if (currWord === correctWord) {
+      setGameOver({ gameOver: true, guessedWord: true });
+      return;
+    }
+    console.log(currAttempt);
+    if (currAttempt.attempt === 5) {
+      setGameOver({ gameOver: true, guessedWord: false });
+      return;
+    }
+    console.log(board)
+  };
 
   return (
-    <div className='app'>
+    <div className="app">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <Board />
-      <Keyboard />
-      
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onEnter,
+          onDelete,
+          correctWord,
+          disabledLetters,
+          setDisabledLetters,
+          gameOver,
+          setGameOver
+        }}
+      >
+        <div className="game">
+          <Board />
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componets/Letter.jsx
+++ src/componets/Letter.jsx
@@ -1,6 +1,30 @@
+/* eslint-disable react/prop-types */
+import { useContext, useEffect } from "react";
+import { AppContext } from "../App";
 
-export default function Letter({letterPos, attemptVal}) {
+export default function Letter({ letterPos, attemptVal }) {
+  const {
+    board,
+    correctWord,
+    currAttempt,
+    setDisabledLetters,
+  } = useContext(AppContext);
+  const letter = board[attemptVal][letterPos];
+
+  const correct = correctWord[letterPos] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+  const letterState =
+    currAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
+
+  useEffect(() => {
+    if (letter !== "" && !correct && !almost) {
+      setDisabledLetters((prev) => [...prev, letter]);
+    }
+  }, [currAttempt.attempt]);
   return (
-    <div>Letter</div>
-  )
+    <div className="letter" id={letterState.toString()}>
+      {letter}
+    </div>
+  );
 }
